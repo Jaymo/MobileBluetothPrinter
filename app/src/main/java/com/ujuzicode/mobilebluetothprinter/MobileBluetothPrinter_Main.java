36208e7c957c2a73941c8af27c40a83902b71eec
Ujuzi Code <james@ujuzi.co.ke>
2018-06-10 20:14:28 +0300

    Activate bluetooth on load spalsh

--- app/src/main/java/com/ujuzicode/mobilebluetothprinter/MobileBluetothPrinter_Main.java
+++ app/src/main/java/com/ujuzicode/mobilebluetothprinter/MobileBluetothPrinter_Main.java
@@ -1,6 +1,35 @@
 package com.ujuzicode.mobilebluetothprinter;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.Menu;
+import android.widget.TextView;
 
-public class MobileBluetothPrinter_Main  extends AppCompatActivity{
+public class MobileBluetothPrinter_Main  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_bluetoth_printer_main);
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        TextView toolbar_title = mToolbar.findViewById(R.id.toolbar_title);
+        toolbar_title.setText(Html.fromHtml("<font color=\"#FFFFFF\"><b>" + getResources().getString(R.string.app_name) + "</b></font>"));
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
 }
--- app/src/main/java/com/ujuzicode/mobilebluetothprinter/MobileBluetothPrinter_Welcome.java
+++ app/src/main/java/com/ujuzicode/mobilebluetothprinter/MobileBluetothPrinter_Welcome.java
@@ -1,9 +1,11 @@
 package com.ujuzicode.mobilebluetothprinter;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MobileBluetothPrinter_Welcome extends AppCompatActivity {
 
@@ -16,6 +18,19 @@ public class MobileBluetothPrinter_Welcome extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+
+                BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+                if (null != adapter) {
+                    if (!adapter.isEnabled()) {
+                        if (adapter.enable()) {
+                            Log.v("MBT","Enabled BluetoothAdapter");
+
+                        } else {
+                            finish();
+                            return;
+                        }
+                    }
+                }
 
                     Intent i = new Intent(MobileBluetothPrinter_Welcome.this,
                             MobileBluetothPrinter_Main.class);
